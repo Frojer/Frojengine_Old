cf7cbd802dfd0e8e2f32eefb7a668c174cf262da
DESKTOP-B5BQ3E3\Frojer <choicoco1995@naver.com>
2017-06-06 22:34:23 +0900

    no message

--- Frojengine/Frojengine/Object/Camera.h
+++ Frojengine/Frojengine/Object/Camera.h
@@ -1,32 +1,46 @@
 #pragma once
 
-#include "..\Frojengine.h"
-
 class CCamera
 {
+protected:
+	LPDEVICE m_pDevice;
+	LPDXDC m_pDXDC;
+
+	D3D11_VIEWPORT m_Viewport;
+
+	float m_FOV;
+	float m_Aspect;
+
 public:
 	VECTOR3 m_Pos;
 	VECTOR3 m_Up;
 	VECTOR3 m_LookAt;
 
-	float m_FOV;
 	float m_Znear;
 	float m_Zfar;
-		
-private:
-	float m_Aspect;
 
+	UINT m_Width;
+	UINT m_Height;
+
+	bool m_Enable;
+	
 public:
 	CCamera();
 	CCamera(const CCamera& obj);
 	~CCamera();
 
-	bool Create(VECTOR3 pos, VECTOR3 up, VECTOR3 lookAt, float fov, float zNear, float zFar, DISPLAYMODE* pMode);
+	bool Create(LPDEVICE pDevice, VECTOR3 pos, VECTOR3 up, VECTOR3 lookAt, float fovAngle, float zNear, float zFar, UINT width, UINT height, D3D11_VIEWPORT& viewport);
 
+	void SetViewport(D3D11_VIEWPORT& viewport);
+
+	void UseCamera();
 	void CameraUpdate();
 
 	MATRIXA GetViewMatrixLH();
 	MATRIXA GetPerspectiveFovLH(DISPLAYMODE mode);
+
+	void SetFOV(float fovAngle) { m_FOV = XMConvertToRadians(fovAngle); }
+	float GetFOV() { return m_FOV; }
 
 	float GetAspect();
 };
